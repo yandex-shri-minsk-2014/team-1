7e662088496c664a01392e7d01bd4bdee53e39be
Sergey Shchurko <klopcool@yandex.by>
2014-12-04 01:49:43 +0300

    Add Registrate

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var config = require('./config')
-require('./server/http-server').start(config.http_server)
+require('./server/http-server').start(config)
 require('./server/socket-server').start(config.socket_server)
--- server/http-server.js
+++ server/http-server.js
@@ -4,86 +4,136 @@ var http = require('http')
   , log = require('npmlog')
   , isStarted = !1
   , path = require('path')
+	, User = require('./db/models/user').User
+	, HttpError = require('../error').HttpError
+	, AuthError = require('./db/models/user').AuthError
+	, mongoose = require('mongoose')
+	, async = require('async')
+  , express = require('express')
+	, bodyParser = require('body-parser')
+	, multer = require('multer')
+	, cookieParser = require('cookie-parser')
+	, session = require('express-session')
+	, MongoStore  = require('connect-mongo')(session)
+	, app = express()
 
 exports.start = function (config) {
   if (config && !isStarted) {
     try {
-      http.createServer(function (request, response) {
-        log.http(request.method + ' request', request.url)
 
-        var urlParsed = url.parse(request.url, true)
+			app.use(bodyParser.json()) // for parsing application/json
+			app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+			app.use(multer()) // for parsing multipart/form-data
+			app.use(cookieParser())
 
-        if (urlParsed.pathname == '/theme' && urlParsed.query.name) {
-          var themePath = 'libs/codemirror/theme/' + urlParsed.query.name
+			app.use(session({
+					secret: config.session.secret
+				, key: config.session.key
+				, cookie: config.session.cookie
+				,	store: new MongoStore ({mongoose_connection: mongoose.connection})
+			}));
 
-          fs.readFile(themePath + '.css', 'utf8',  function (err, data) {
-            if (err) throw err
+			app.get('/theme', function(request, response) {
+				var urlParsed = url.parse(request.url, true)
 
-            response.write(JSON.stringify(data))
-            response.end()
-          })
-        }
-        else if (urlParsed.pathname == '/theme' && !urlParsed.query.name) {
-          fs.readdir('libs/codemirror/theme/', function (err, files) {
-            if (err) throw err
+				if (urlParsed.query.name) {
+					var themePath = 'libs/codemirror/theme/' + urlParsed.query.name
 
-            response.write(JSON.stringify(files))
-            response.end()
-          })
-        }
-        else if (request.method == 'POST') {
-          var body = ''
-          request.on('data', function (data) {
-              body += data
-          });
-          request.on('end', function () {
-            var jsonBody = JSON.parse(body)
-            if (jsonBody.operation == 'save') {
-              saveDocument(jsonBody)
-            }
-            else if (jsonBody.operation == 'get') {
-              //console.log('getDocument ' + jsonBody.docName)
-              var docContent = getDocument(jsonBody.docName)
-              //console.log(docContent)
-              var docObj = {
-                value: docContent
-              }
+					fs.readFile(themePath + '.css', 'utf8',  function (err, data) {
+						if (err) throw err
 
-              var docJSON = JSON.stringify(docObj)
-              
-              if (docJSON != null) {
-                //response.writeHead(200, { 'Content-Type': 'application/json' })
-                console.log(docJSON)
-                //response.write(docJSON)
-                response.end(docJSON)
-              }
-              else {
-                console.log('nothing');
-                response.end()
-              }
-              
-            }
-            
-          });
-        } 
-        else {
-          //reading index file
-          fs.readFile(config.index, function (err, page) {
-            if (err) {
-              log.error('HTTP server', err.message)
-              response.writeHeader(500)
-              response.end('Can\'t read ' + config.index +
-                           ' file. (Try to create it: npm run make)')
-              return
-            }
+						response.send(JSON.stringify(data))
+					})
+				}
+				else if (!urlParsed.query.name) {
+					fs.readdir('libs/codemirror/theme/', function (err, files) {
+						if (err) throw err
 
-            response.writeHeader(200, {'Content-Type': 'text/html'})
-            response.write(page)
-            response.end()
-          })
-        }
-      }).listen(config.port)
-      log.info('HTTP server', 'Server started at port ' + config.port)
+						response.send(JSON.stringify(files))
+					})
+				}
+			})
+
+			app.post('/login', function(request, response, next) {
+
+				var username = request.body.username;
+				var password = request.body.password;
+
+				User.authorize(username, password, function(err, user) {
+					if (err) {
+						if (err instanceof AuthError) {
+							response.status(403).send('Неверный пароль');
+							return next(new HttpError(403, err.message));
+						} else {
+							response.redirect('/')
+							return next(err);
+						}
+					}
+
+					request.session.user = user._id;
+					response.status(200).send(user.username);
+
+				});
+
+			})
+
+			app.post('/', function(request, response) {
+				var body = ''
+				request.on('data', function (data) {
+					body += data
+				});
+				request.on('end', function () {
+					var jsonBody = JSON.parse(body)
+					if (jsonBody.operation == 'save') {
+						saveDocument(jsonBody)
+					}
+					else if (jsonBody.operation == 'get') {
+						//console.log('getDocument ' + jsonBody.docName)
+						var docContent = getDocument(jsonBody.docName)
+						//console.log(docContent)
+						var docObj = {
+							value: docContent
+						}
+						var docJSON = JSON.stringify(docObj)
+
+						if (docJSON != null) {
+							//response.writeHead(200, { 'Content-Type': 'application/json' })
+							console.log(docJSON)
+							//response.write(docJSON)
+							response.send(docJSON)
+						}
+						else {
+							console.log('nothing');
+							response.send()
+						}
+					}
+				});
+			})
+
+			app.get('/', function(request, response, next) {
+				fs.readFile(config.http_server.index, function (err, page) {
+					if (err) {
+						log.error('HTTP server', err.message)
+						response.status(500).send('Can\'t read ' + config.http_server.index + ' file.')
+						return
+					}
+
+					response.set('Content-Type', 'text/html');
+					response.status(200).send(page)
+
+				})
+			})
+
+			app.use(function(err, request, response, next) {
+				log.error('HTTP server', err.message)
+			})
+
+			var server = app.listen(config.http_server.port, function () {
+				var host = server.address().address
+				var port = server.address().port
+				console.log('Example app listening at http://%s:%s', host, port)
+			})
+      log.info('HTTP server', 'Server started at port ' + config.http_server.port)
       isStarted = !0
     } catch (e) {
       log.error('HTTP server', 'Server can\'t start. ' + e)
